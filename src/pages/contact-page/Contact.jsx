06adbd5522ead4b6ac27b8e6d961ced37d4b24d2
EldostJs <mirzeyev0308@gamil.com>
2024-10-25 01:33:59 +0400

    fix

--- src/pages/contact-page/Contact.jsx
+++ src/pages/contact-page/Contact.jsx
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
 import styles from './Contact.module.css';
-import { Mail, Phone, MapPin, Linkedin, Github, Instagram, Facebook } from 'lucide-react'; // Импортируем иконки из lucide-react
+import { Mail, Phone, Linkedin, Github, Instagram, Facebook } from 'lucide-react';
 import im from './images/contactsYellow.svg';
 
-
-const mailClick = () => {
-
-    const email = 'mirzeyev0308@gmail.com'
-    const subject = 'Frontend Developer Job Opportunity'
-    const body = 'Hello, Eldost! We are interested in your profile as a Frontend Developer (JS, React) and would like to discuss a job opportunity at our company. Please provide details about your experience and job preferences. Thank you for your response!'
-    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-    window.location.href = mailtoLink
-
-}
-
-const whatsappClick = () => {
-
-    const phoneNumber = '+994503272087';
-    const message = 'Hello, Eldost! We are interested in your profile as a Frontend Developer (JS, React) and would like to discuss a job opportunity at our company. Please provide details about your experience and job preferences. Thank you for your response!'
-    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`
-    window.location.href = whatsappLink
-}
-
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const [isSending, setIsSending] = useState(false); 
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setIsSending(true); 
+
+        emailjs.send(
+            'service_gr8ksem', // Service ID
+            'template_wh5j59i', //Template ID
+            formData,
+            'POEPMEYT4RKS8Wnfq' //Public Key
+        )
+            .then((result) => {
+                console.log('Email успешно отправлен!', result.text);
+                toast.success('Message sent successfully!'); 
+                setIsSending(false); 
+            }, (error) => {
+                console.error('Ошибка при отправке email:', error.text);
+                toast.error('Error sending message.'); 
+                setIsSending(false);
+            });
+
+        setFormData({
+            name: '',
+            email: '',
+            message: ''
+        });
+    };
+
+    const mailClick = () => {
+        const email = 'mirzeyev0308@gmail.com';
+        const subject = 'Frontend Developer Job Opportunity';
+        const body = 'Hello, Eldost! We are interested in your profile as a Frontend Developer (JS, React) and would like to discuss a job opportunity at our company. Please provide details about your experience and job preferences. Thank you for your response!';
+        const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+        window.location.href = mailtoLink;
+    };
+
+    const whatsappClick = () => {
+        const phoneNumber = '+994503272087';
+        const message = 'Hello, Eldost! We are interested in your profile as a Frontend Developer (JS, React) and would like to discuss a job opportunity at our company. Please provide details about your experience and job preferences. Thank you for your response!';
+        const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+        window.location.href = whatsappLink;
+    };
+
     const socialLinks = [
         { platform: 'GitHub', icon: <Github />, url: 'https://github.com/EldostJs' },
         { platform: 'LinkedIn', icon: <Linkedin />, url: 'https://www.linkedin.com/in/eldostmirzeyev/' },
         { platform: 'Instagram', icon: <Instagram />, url: 'https://www.instagram.com/_mirzeev_/?igsh=MThrcDJzdXp2Y2h4ZQ%3D%3D' },
         { platform: 'Facebook', icon: <Facebook />, url: 'https://www.facebook.com/eldost.mirzoev?locale=ru_RU' },
-
-
     ];
 
     return (
@@ -38,12 +79,14 @@ const Contact = () => {
 
             <div className={styles.contentWrapper}>
                 <div className={styles.formWrapper}>
-                    <div className={styles.formSection}>
+                    <form onSubmit={handleSubmit} className={styles.formSection}>
                         <div className={styles.inputGroup}>
                             <input
                                 required
                                 type="text"
-                                name="text"
+                                name="name"
+                                value={formData.name}
+                                onChange={handleChange}
                                 autoComplete="off"
                                 className={styles.input}
                             />
@@ -55,6 +98,8 @@ const Contact = () => {
                                 required
                                 type="email"
                                 name="email"
+                                value={formData.email}
+                                onChange={handleChange}
                                 autoComplete="off"
                                 className={styles.input}
                             />
@@ -65,34 +110,33 @@ const Contact = () => {
                             <textarea
                                 required
                                 name="message"
+                                value={formData.message}
+                                onChange={handleChange}
                                 autoComplete="off"
                                 className={`${styles.input} ${styles.textarea}`}
                             />
                             <label className={styles.userLabel}>Message</label>
                         </div>
 
-                        <button className={styles.sendButton}>
-                            Send
+                        <button type="submit" className={styles.sendButton} disabled={isSending}>
+                            {isSending ? 'Sending...' : 'Send'}
                             <span className={styles.sendIcon}>➤</span>
                         </button>
-                    </div>
+                    </form>
                 </div>
 
                 <div className={styles.infoWrapper}>
                     <div className={styles.contactInfo}>
+                        <p className={styles.contactText}>You can also reach me via:</p>
                         <div className={styles.infoItem}>
-                            <button
-                                onClick={mailClick}
-                                className={styles.socialButton} aria-label="Email">
+                            <button onClick={mailClick} className={styles.socialButton} aria-label="Email">
                                 <Mail />
                             </button>
-                            <span>janedoe.test@gmail.com</span>
+                            <span>mirzeyev0308@gmail.com</span>
                         </div>
 
                         <div className={styles.infoItem}>
-                            <button
-                                onClick={whatsappClick}
-                                className={styles.socialButton} aria-label="Phone">
+                            <button onClick={whatsappClick} className={styles.socialButton} aria-label="Phone">
                                 <Phone />
                             </button>
                             <span>+994-50-327-20-87</span>
@@ -114,9 +158,11 @@ const Contact = () => {
                 </div>
 
                 <div className={styles.imageWrapper}>
-                    <img src={im} alt="Contact" className={styles.contactImage} />
+                    <img src={im} alt="" className={styles.contactImage} />
                 </div>
             </div>
+
+            <ToastContainer />
         </div>
     );
 };
