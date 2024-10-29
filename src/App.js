5c77c52adf6cadfea1def0213e35cddad80d5387
EldostJs <mirzeyev0308@gamil.com>
2024-10-30 02:32:56 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import ReactGA4 from 'react-ga4';
+import React from 'react';
 import { LanguageProvider } from './translate/LanguageContext';
 import Home from "./pages/home-page/Home";
 import About from "./pages/about-page/About";
@@ -10,28 +9,6 @@ import Skills from "./pages/skills-page/Skills";
 import Contact from "./pages/contact-page/Contact";
 
 function App() {
-    useEffect(() => {
-        // Initialize GA4
-        ReactGA4.initialize('G-0Q151VQPKQ');
-        
-        // Send pageview with a callback
-        ReactGA4.send({
-            hitType: "pageview",
-            page: window.location.pathname + window.location.search
-        });
-
-        // Optional: Track page changes if you're using React Router
-        const handleLocationChange = () => {
-            ReactGA4.send({
-                hitType: "pageview",
-                page: window.location.pathname + window.location.search
-            });
-        };
-
-        window.addEventListener('popstate', handleLocationChange);
-        return () => window.removeEventListener('popstate', handleLocationChange);
-    }, []);
-
     return (
         <LanguageProvider>
             <Home />
